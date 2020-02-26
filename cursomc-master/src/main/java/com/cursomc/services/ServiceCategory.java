b4b85b0594816809303c83a3371387d6e4a04cea
guimaarques <53923371+guimaarques@users.noreply.github.com>
2020-02-26 18:03:19 -0300

    changes made at work

---
+++ cursomc-master/src/main/java/com/cursomc/services/ServiceCategory.java
@@ -0,0 +1,23 @@
+package com.cursomc.services;
+
+import com.cursomc.domain.Categoria;
+import com.cursomc.repositories.CategoriaRepository;
+import com.cursomc.services.exceptions.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class ServiceCategory {
+	
+	@Autowired
+	private CategoriaRepository repo;
+
+	public Categoria find(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+	}
+	
+}
