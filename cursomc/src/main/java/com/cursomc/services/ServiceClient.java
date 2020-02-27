3ec159b9e7990b01127f5efdada44d986d17bb7e
guimaarques <53923371+guimaarques@users.noreply.github.com>
2020-02-27 16:27:28 -0300

    all service

---
+++ cursomc/src/main/java/com/cursomc/services/ServiceClient.java
@@ -0,0 +1,23 @@
+package com.cursomc.services;
+
+import com.cursomc.domain.Cliente;
+import com.cursomc.repositories.ClienteRepository;
+import com.cursomc.services.exceptions.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class ServiceClient {
+	
+	@Autowired
+	private ClienteRepository repo;
+
+	public Cliente find(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+	
+}
