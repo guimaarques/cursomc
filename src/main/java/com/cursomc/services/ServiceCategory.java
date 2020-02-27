81ce9fc1e16f50b1864b027c43aa408221addf14
Guilherme Marques <guih_marques@hotmail.com>
2020-02-26 22:27:03 -0300

    fixing version problem

--- src/main/java/com/cursomc/services/ServiceCategory.java
+++ src/main/java/com/cursomc/services/ServiceCategory.java
@@ -1,23 +1,23 @@
 package com.cursomc.services;
 
-import java.util.Optional;
-
+import com.cursomc.domain.Categoria;
+import com.cursomc.repositories.CategoriaRepository;
+import com.cursomc.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.cursomc.domain.Categoria;
-import com.cursomc.repositories.CategoriaRepository;
+import java.util.Optional;
 
 @Service
 public class ServiceCategory {
 	
 	@Autowired
 	private CategoriaRepository repo;
-	
+
 	public Categoria find(Integer id) {
-		
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 	
 }
