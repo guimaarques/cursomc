f9fe6dedf1eb5b57ef123a4f9826848959f1a551
guimaarques <53923371+guimaarques@users.noreply.github.com>
2020-02-27 13:14:51 -0300

    new_changes_on_master

---
+++ src/main/java/com/cursomc/domain/Cidade.java
@@ -0,0 +1,68 @@
+package com.cursomc.domain;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+public class Cidade implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String cidade;
+
+    @JsonManagedReference
+    @ManyToOne
+    @JoinColumn(name = "estado_id")
+    private Estado estado;
+
+    public Cidade() {
+    }
+
+    public Cidade(Integer id, String cidade, Estado estado) {
+        this.id = id;
+        this.cidade = cidade;
+        this.estado = estado;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public Estado getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Estado estado) {
+        this.estado = estado;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cidade cidade = (Cidade) o;
+        return Objects.equals(id, cidade.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
